3ffb98c086a39def00d97075297eb574808c5588
James Mudidi <mudidi.jimmy@gmail.com>
2019-06-30 06:27:43 +0300

    setup error page

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -3,25 +3,13 @@ import { Alert } from 'bootstrap-4-react';
 
 export default class PageNotFound extends Component {
   render() {
-    return <Alert secondary>Primary Alert</Alert>;
+    return (
+      <Alert alert-light justify-content-sm-center>
+        404
+        <br />
+        {' '}
+        The page you have requested for does not exist
+      </Alert>
+    );
   }
 }
-
-// const PageNotFound = () => (
-//   <div className="container">
-//     <div className="copy-container error-space">
-//       <p className="error">
-//         404
-//       </p>
-//       <br />
-//       <p className="error1">
-//         Page not found.
-//       </p>
-//       <p className="error2">
-//         <a href="#back">Go back</a>
-//       </p>
-//     </div>
-//   </div>
-
-// );
-// export default PageNotFound;
